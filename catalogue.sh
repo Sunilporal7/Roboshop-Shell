7491c3b7dbe98f57c4e12c15351f25a69cca977e
sunil <sunilporal713@gmail.com>
2023-08-25 11:02:20 +0530

    new lines added

--- catalogue.sh
+++ catalogue.sh
@@ -16,4 +16,4 @@ systemctl enable catalogue
 systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.devopsroboshop.online </app/schema/catalogue.js

d276356918dc9644180bc4245e15ec94e45e8284
sunil <sunilporal713@gmail.com>
2023-08-27 14:39:05 +0530

    new lines added

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,51 @@
 script_location=$(pwd)
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir -p /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.devopsroboshop.online </app/schema/catalogue.js
+LOG=/tmp/roboshop.log
+
+echo -e "\e[35m configuring Nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+
+echo -e "\e[35m install Nodejs repos\e[0m"
+yum install nodejs -y &>>${LOG}
+
+echo -e "\e[35m Add application user\e[0m"
+useradd roboshop &>>${LOG}
+
+echo -e "\e[35m making directory\e[0m"
+mkdir -p /app &>>${LOG}
+
+echo -e "\e[35m Downloading app content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+
+echo -e "\e[cleanup old content \e[0m"
+rm -rf /app/* &>>${LOG}
+
+echo -e "\e[Extracting app content\e[0m"
+cd /app &>>${LOG}
+unzip /tmp/catalogue.zip &>>${LOG}
+
+echo -e "\e[35m making directory\e[0m"
+cd /app &>>${LOG}
+
+
+npm install &>>${LOG}
+
+
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
+
+
+systemctl daemon-reload &>>${LOG}
+
+
+systemctl enable catalogue &>>${LOG}
+
+
+systemctl start catalogue &>>${LOG}
+
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+
+
+yum install mongodb-org-shell -y &>>${LOG}
+
+
+mongo --host mongodb-dev.devopsroboshop.online </app/schema/catalogue.js &>>${LOG}

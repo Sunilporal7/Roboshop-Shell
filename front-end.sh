402531e7aae42d7c25d1ff8c75ea9a076853cb27
sunil <sunilporal713@gmail.com>
2023-08-27 12:00:44 +0530

    new lines added

--- front-end.sh
+++ front-end.sh
@@ -3,33 +3,73 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[35m install nginx\e[0m"
 yum install nginx -y &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 echo -e "\e[35m enable nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 echo -e "\e[35m start nginx\e[0m"
 systemctl start nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 echo -e "\e[35m remove nginx old file\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 echo -e "\e[35m copy nginx old file\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 echo -e "\e[35m unzip frontend\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
-echo -e "\e[copy nginx config file\e[0m"
+echo -e "\e[35m copy nginx config file\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi
 
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+then
+  echo SUCCESS
+else
+  echo FAILURE
+  fi

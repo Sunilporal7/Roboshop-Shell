db781bc05b900d5b6daaf8313e8e320e3f7d8e67
sunil <sunilporal713@gmail.com>
2023-08-29 16:05:43 +0530

    new lines added

---
+++ Redis.sh
@@ -0,0 +1,22 @@
+source common.sh
+print_head "setup Redis repos"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
+status_check
+
+print_head "Enabling Redis 6.2 dnf module"
+yum module enable redis:remi-6.2 -y &>>${LOG}
+status_check
+
+print_head "Redis install "
+yum install redis -y &>>${LOG}
+status_check
+
+print_head "update Redis listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+status_check
+
+systemctl enable Redis &>>${LOG}
+status_check
+
+systemctl start Redis &>>${LOG}
+status_check

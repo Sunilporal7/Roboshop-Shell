d71e6a733ee7a03bf28f64c46524b61d87d0e6a0
sunil <sunilporal713@gmail.com>
2023-08-29 11:08:29 +0530

    new lines added

--- mongodb.sh
+++ mongodb.sh
@@ -4,19 +4,19 @@ print_head "copy Mongo DB Repo file"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
 
-print_head "install Mongo DB"
-yum install mongodb-org -y &>>$(LOG)
+print_head "Install Mongo DB"
+yum install mongodb-org -y &>>${LOG}
 status_check
 
-systemctl enable mongod &>>$(LOG)
+systemctl enable mongod &>>${LOG}
 status_check
 
-systemctl start mongod &>>$(LOG)
+systemctl start mongod &>>${LOG}
 status_check
 
 print_head "update Mongo DB listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$(LOG)
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 status_check
 
-systemctl restart mongod &>>$(LOG)
+systemctl restart mongod &>>${LOG}
 status_check

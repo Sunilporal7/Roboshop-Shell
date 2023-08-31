f7403a3cbeab33546727b402afd4777f481e0e69
sunil <sunilporal713@gmail.com>
2023-08-31 09:48:38 +0530

    new lines added

--- common.sh
+++ common.sh
@@ -14,6 +14,84 @@ print_head () {
   echo -e "\e[1m $1 \e[0m"
 }
 
+app_prereq(){
+
+  print_head "Add application user"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]
+  then
+  useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  print_head "making directory"
+  mkdir -p /app &>>${LOG}
+  status_check
+
+  print_head "Downloading app content"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "cleanup old content "
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  print_head "Extracting app content"
+  cd /app &>>${LOG}
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
+  }
+
+systemd_setup(){
+
+  print_head "configure ${component}.service"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "Demon reload"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component}"
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component}"
+  systemctl start ${component} &>>${LOG}
+  status_check
+}
+
+load_schema(){
+ if [ ${schema_load} == "true" ]
+ then
+   if [ ${schema_type} == "mongo" ]
+   then
+  print_head "Configure mongo repo"
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+  status_check
+
+  print_head "Install mongo client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "load schema"
+  mongo --host mongodb-dev.devopsroboshop.online </app/schema/${component}.js &>>${LOG}
+  status_check
+  fi
+ fi
+
+ load_schema(){
+  if [ ${schema_load} == "mysql" ]
+  then
+    print_head "install mysql server"
+    yum install mysql -y &>>${LOG}
+    status_check
+
+     print_head "load schema"
+        mysql -h mysql-dev.devopsroboshop.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+        status_check
+
+}
 Nodejs(){
 print_head "configuring Nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
@@ -23,64 +101,36 @@ print_head "install Nodejs repos"
 yum install nodejs -y &>>${LOG}
 status_check
 
-print_head "Add application user"
-id roboshop &>>${LOG}
-if [ $? -ne 0 ]
-then
-useradd roboshop &>>${LOG}
-fi
-status_check
-
-print_head "making directory"
-mkdir -p /app &>>${LOG}
-status_check
-
-print_head "Downloading app content"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
-status_check
-
-print_head "cleanup old content "
-rm -rf /app/* &>>${LOG}
-status_check
-
-print_head "Extracting app content"
-cd /app &>>${LOG}
-unzip /tmp/${component}.zip &>>${LOG}
-status_check
+app_prereq
 
 print_head "installing nodejs dependecies"
 cd /app &>>${LOG}
 npm install &>>${LOG}
 status_check
 
-print_head "configure ${component}.service"
-cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
-status_check
+systemd_setup
 
-print_head "Demon reload"
-systemctl daemon-reload &>>${LOG}
-status_check
 
-print_head "Enable ${component}"
-systemctl enable ${component} &>>${LOG}
-status_check
+}
 
-print_head "Start ${component}"
-systemctl start ${component} &>>${LOG}
-status_check
+maven(){
 
-if [ ${schema_load} == "true" ]
-then
- print_head "Configure mongo repo"
- cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+ print_head "install maven"
+ yum install maven -y &>>${LOG}
  status_check
 
- print_head "Install mongo client"
- yum install mongodb-org-shell -y &>>${LOG}
- status_check
+app_prereq
 
- print_head "load schema"
- mongo --host mongodb-dev.devopsroboshop.online </app/schema/${component}.js &>>${LOG}
- status_check
-fi
+print_head "build a package"
+mvn clean package
+status_check
+
+print_head "copy app file to location"
+mv target/${component}-1.0.jar ${component}.jar
+status_check
+
+systemd_setup
+
+load_schema
+
 }
---
+++ shipping.sh
@@ -0,0 +1,13 @@
+source common.sh
+
+if [ -z "${root_mysql_password}" ]
+then
+  echo"variable mysql root password is nedded"
+  exit
+  fi
+
+component=shipping
+schema_load=true
+schema_type=mysql
+
+maven
